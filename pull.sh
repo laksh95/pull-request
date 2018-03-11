b5560260e3e76ff92c75aca9f2767805f82c4011
laksh95 <goel.laksh95@gmail.com>
2018-03-11 20:26:13 +0530

    added the dynamic repo url

--- pull.sh
+++ pull.sh
@@ -1,5 +1,5 @@
 ref=$(git symbolic-ref HEAD | cut -d'/' -f3)
-repo=`git remote -v | grep -m 1 origin | sed "s/git@github.com://" | sed "s/\.git//"`
+repo=`git config --get remote.origin.url | sed "s/git@github.com://" | sed "s/\.git//"`
 echo opening pull request for $ref on $repo
 branch=""
 if [ $1 ]; then
@@ -8,4 +8,6 @@ else
 	branch="develop...$ref"
 fi
 echo $branch
-open -a /Applications/Firefox\ 2.app http://github.com/$repo/compare/$branch?expand=1
+echo $repo
+#open -a /Applications/Firefox\ 2.app http://github.com/$repo/compare/$branch?expand=1
+open -a /Applications/Google\ Chrome.app http://github.com/$repo/compare/$branch?expand=1 
